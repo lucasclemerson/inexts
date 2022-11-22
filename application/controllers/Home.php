bfed2872714a0a2c617585d12b5876b1c7479bb5
Clemerson Lucas <87621463+lucasclemerson@users.noreply.github.com>
2022-11-22 11:44:35 -0300

    Update and rename Welcome.php to Home.php
    Criação do homepage

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,7 +1,7 @@
 <?php
     defined('BASEPATH') OR exit('No direct script access allowed');
   
-    class Welcome extends CI_Controller {
+    class Home extends CI_Controller {
 
         public function __construct(){
             parent::__construct();
@@ -10,10 +10,16 @@
         }   
     
         public function index (){
-            if ($_GET['mostrar_mensagem']==1){
+            $dados['page'] = 'home';
+            $dados['mostrar_mensagem']=false;
+            $dados['conteudo_mensagem']="";
+            
+            
+            if (isset($_GET['mostrar_mensagem'])){
                 $dados['mostrar_mensagem']=true;
-                $dados['conteudo_mensagem']="E-mail enviado com sucesso!";
+                $dados['conteudo_mensagem']=$_GET['mostrar_mensagem']==1?"E-mail enviado com sucesso!":"Problemas com o envio do E-mail!";
             }
+            
             $this->load->view('index', $dados);
         }
     }
