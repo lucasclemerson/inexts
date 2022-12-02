ea184fae37978c05118ad29691b374328453dfd0
Clemerson Lucas <87621463+lucasclemerson@users.noreply.github.com>
2022-12-02 14:39:17 -0300

    Create footer.php
    Criação do footer

---
+++ application/views/admin/footer.php
@@ -0,0 +1,7 @@
+<div style="height:4px;" class="bg-warning"></div>
+<footer class="footer">
+  	<strong> INEXTS</strong>| <strong>Inovação & Tecnologia</strong>| &copy; 2021 - <?php echo Date('Y');?>,
+  	<a href="<?=base_url()?>"
+      	target="_self" class="text-orange"><strong>Todos os direitos reservados.</strong>
+    </a>
+</footer>
